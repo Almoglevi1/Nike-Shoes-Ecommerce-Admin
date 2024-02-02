15152b3bbd84d5f37c93c2a85f1bacae1a828359
Almog Levi <almoglevi71@gmail.com>
2024-02-02 18:11:39 +0200

    Updating project

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -1,12 +1,11 @@
 import Layout from "@/components/Layout";
-import ProductForm from "@/components/ProductForm";
-import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DeleteProductPage() {
-    const [productInfo, setProductInfo] = useState(null);
     const router = useRouter();
+    const [productInfo, setProductInfo] = useState();
     const { id } = router.query;
 
     useEffect(() => {
@@ -29,10 +28,19 @@ export default function DeleteProductPage() {
 
     return (
         <Layout>
-            <h1 className="text-center">Do you really want to delete &nbsp;"{productInfo?.title}"?</h1>
-            <div className="flex gap-2 justify-center">
-                <button className="btn-red" onClick={deleteProduct}>Yes</button>
-                <button className="btn-default" onClick={goBack}>No</button>
+            <h1 className={"text-center"}>Do you really want to delete
+                &nbsp;"{productInfo?.title}"?</h1>
+            <div className={"flex gap-2 justify-center"}>
+                <button
+                    className={"btn-red"}
+                    onClick={deleteProduct}>
+                    Yes
+                </button>
+                <button
+                    className={"btn-default"}
+                    onClick={goBack}>
+                    No
+                </button>
             </div>
         </Layout>
     );
